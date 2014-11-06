e7e5a677581929df51efc66e561e511c813c8aa0
Zoltan Nagy <abesto@abesto.net>
2014-11-06 15:40:40 +0100

    Saghetti -> 2.1-alpha-1

--- spaghetti.rb
+++ spaghetti.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Spaghetti < Formula
   homepage 'https://github.com/prezi/spaghetti'
-  version "2.0"
-  sha1 '17ebb64f91ed7fe389843825b09bb68423bd4cf3'
+  version "2.1-alpha-1"
+  sha1 'b7792de6c16305b59ffb64c5e2dfd2e321f77d15'
   url "https://github.com/prezi/spaghetti/releases/download/#{version}/spaghetti-#{version}.zip"
 
   def install
