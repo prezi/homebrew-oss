321c074b49fee3af8e4fd183eb84f14eabda61e8
Lorant Pinter <lorant.pinter@prezi.com>
2014-12-02 18:23:33 +0100

    Upgraded to Pride 0.9.14

--- pride.rb
+++ pride.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Pride < Formula
   homepage "https://github.com/prezi/pride/"
-  version "0.9.13"
+  version "0.9.14"
   url "https://oss.sonatype.org/content/repositories/releases/com/prezi/gradle/pride/pride/#{version}/pride-#{version}-dist.zip"
-  sha1 "f886bb68c2165cd8ec73dae379cd77ba7101d9ab"
+  sha1 "0d964c9a64b7722d92421634d0c17eb78dcf8009"
 
   def install
     libexec.install ["pride-#{version}/bin", "pride-#{version}/lib"]
