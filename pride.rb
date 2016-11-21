4d9f1fd1caf5923f712eda721f0aae9f346da229
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2016-11-21 10:12:44 +0100

    Replaced sha1 with sha256 for pride

--- pride.rb
+++ pride.rb
@@ -4,7 +4,7 @@ class Pride < Formula
   homepage "https://github.com/prezi/pride/"
   version "0.10"
   url "https://oss.sonatype.org/content/repositories/releases/com/prezi/pride/pride/#{version}/pride-#{version}-dist.zip"
-  sha1 "202f68b3da9e6d62019c03d0334ef4f5f34464d0"
+  sha256 "2b14ff2bc22b66a76be9ad535bae2248811488d1313efeaf6e89b165f8466686"
 
   def install
     libexec.install ["pride-#{version}/bin", "pride-#{version}/lib"]
