4af5cd36cd2d03df73e245de35ea61b7b35992f5
rosadam <rosadam@gmail.com>
2017-01-03 13:31:45 +0100

    Update pride to 0.11

--- pride.rb
+++ pride.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Pride < Formula
   homepage "https://github.com/prezi/pride/"
-  version "0.10"
+  version "0.11"
   url "https://oss.sonatype.org/content/repositories/releases/com/prezi/pride/pride/#{version}/pride-#{version}-dist.zip"
-  sha256 "2b14ff2bc22b66a76be9ad535bae2248811488d1313efeaf6e89b165f8466686"
+  sha256 "ee85c365f049e719f996aba49cb41dd681cf2bcf7350c0f3ce18d78f2505ddb3"
 
   def install
     libexec.install ["pride-#{version}/bin", "pride-#{version}/lib"]
